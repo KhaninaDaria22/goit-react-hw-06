567a1520e8057123d71d1ca268324b1128365889
Daria Khanina <khanina_dasha@ukr.net>
2024-03-20 23:45:22 +0200

    commit 5

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' 
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"; import storage from 'redux-persist/lib/storage' 
 
 const contactPersistConfig = {
   key: 'contact',
@@ -19,6 +26,13 @@ export const store = configureStore({
     contacts: persistedContactReducer,
     filters: filtersReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
