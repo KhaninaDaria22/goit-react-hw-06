28ba28fca181bae9bbd158217cf9282f19b91818
Daria Khanina <khanina_dasha@ukr.net>
2024-03-23 17:45:42 +0200

    commit 17

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,13 +1,19 @@
 import css from "../css/ContactList.module.css";
 import Contact from "./Contact"
 import {  useSelector } from "react-redux";
+import { selectFilter } from "../redux/filtersSlice";
 
 const ContactList = () => {
-  const contacts = useSelector(state => state.contacts.contacts);
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const filter = useSelector(selectFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <ul className={css.ulContacts}>
-      {contacts.map(({ id, name, number }) => (
+      {filteredContacts.map(({ id, name, number }) => (
         <Contact key={id} id={id} name={name} number={number} />
       ))}
     </ul>
